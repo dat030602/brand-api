1d50191341d0a3ed66e1aa8cb1ed8115b54e972f
tai <tainguyensanh2@gmail.com>
2023-07-14 17:36:28 +0700

    update db

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,36 +1,57 @@
 const config = require("../DbConfig");
 const sql = require("mssql");
 class HomeController {
-	async index(req, res) {}
-	// [GET] /home/getBranch1
-	getBranch1(req, res) {
-		// const func = async () => {
-		// 	try {
-		// 		let result;
-		// 		await sql.connect(config.config).then((conn) =>
-		// 			conn
-		// 				.request()
-		// 				.query(
-		// 					`SELECT TOP 8 DT.MaDoiTac, Dt.TenDoiTac, DT.LoaiAmThuc,TD.Rating
-		// 					FROM dbo.DOITAC DT, dbo.THUCDON TD
-		// 					where DT.MaDoiTac = TD.MaDoiTac`
-		// 				)
-		// 				.then((v) => {
-		// 					result = v;
-		// 				})
-		// 				.then(() => conn.close())
-		// 		);
-		// 		return result;
-		// 	} catch (error) {
-		// 		console.log(`Error: ${error}`);
-		// 	}
-		// };
-		// func().then((resReturn) => {
+  async index(req, res) {
+    const func = async () => {
+      try {
+        let result;
+        await sql.connect(config.config).then((conn) =>
+          conn
+            .request()
+            .query(`select * from sanpham`)
+            .then((v) => {
+              result = v;
+            })
+            .then(() => conn.close())
+        );
+        return result;
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+    };
+    func().then((resReturn) => {
+      res.json(resReturn.recordset);
+    });
+  }
+  // [GET] /home/getBranch1
+  getBranch1(req, res) {
+    // const func = async () => {
+    // 	try {
+    // 		let result;
+    // 		await sql.connect(config.config).then((conn) =>
+    // 			conn
+    // 				.request()
+    // 				.query(
+    // 					`SELECT TOP 8 DT.MaDoiTac, Dt.TenDoiTac, DT.LoaiAmThuc,TD.Rating
+    // 					FROM dbo.DOITAC DT, dbo.THUCDON TD
+    // 					where DT.MaDoiTac = TD.MaDoiTac`
+    // 				)
+    // 				.then((v) => {
+    // 					result = v;
+    // 				})
+    // 				.then(() => conn.close())
+    // 		);
+    // 		return result;
+    // 	} catch (error) {
+    // 		console.log(`Error: ${error}`);
+    // 	}
+    // };
+    // func().then((resReturn) => {
 
-		// 	res.json(resReturn.recordset);
-		// });
-    res.json({})
-	}
+    // 	res.json(resReturn.recordset);
+    // });
+    res.json({});
+  }
 }
 
 module.exports = new HomeController();
