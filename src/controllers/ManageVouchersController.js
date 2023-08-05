0ae75a4e4898508ed903806151e1f9f7dbfdda53
dat123 <dat030602@gmail.com>
2023-08-05 20:02:47 +0700

    Manage Voucher

--- src/controllers/ManageVouchersController.js
+++ src/controllers/ManageVouchersController.js
@@ -1,10 +1,6 @@
-require("../FirebaseConfig");
-const firebase = require("firebase/storage");
 const uuid = require("uuid");
 const config = require("../DbConfig");
 const sql = require("mssql");
-
-const storage = firebase.getStorage();
 
 class ManageVouchersController {
 	async index(req, res) {}
@@ -16,7 +12,7 @@ class ManageVouchersController {
 				await sql.connect(config).then((conn) =>
 					conn
 						.request()
-						.query(`select * from voucher`)
+						.query(`select * from voucher;select * from v_voucherUsed`)
 						.then((v) => {
 							result = v;
 						})
@@ -28,58 +24,28 @@ class ManageVouchersController {
 			}
 		};
 		func().then((response) => {
-			res.json(response?.recordset)
+			if (response?.recordsets === undefined) res.status(400);
+			else if (response.recordsets.length === 0) res.status(400);
+			else
+				res.json(
+					response.recordsets[0].map((el) => {
+						return {
+							voucher: el,
+							history: response.recordsets[1].filter((el1) => {
+								return el.voucher_id === el1.voucher_id;
+							}),
+						};
+					})
+				);
 		});
 	}
-	
 	// [POST]
 	AddVoucher(req, res) {
-		const body = JSON.parse(req.body.data);
-		async function UploadImage(file) {
-			try {
-				const storageRef = firebase.ref(
-					storage,
-					`Image/${file.originalname.slice(0, file.originalname.length - 4) + uuid.v4()}`
-				);
-				const metadata = {
-					contentType: file.mimetype,
-					name: file.originalname,
-				};
-				const snapshot = await firebase.uploadBytesResumable(storageRef, file.buffer, metadata);
-				const downloadURL = await firebase.getDownloadURL(snapshot.ref);
-				return downloadURL;
-			} catch (error) {
-				return "";
-			}
-		}
-
-		async function AddDetail(element, index, id) {
-			try {
-				var requestDetail = new sql.Request();
-				const downloadURL = await UploadImage(req.files["imageUpload"][index]);
-				requestDetail.input(`masp`, sql.VarChar(10), id);
-				requestDetail.input(`stt`, sql.Int, index + 1);
-				requestDetail.input(`tenctsp`, sql.NVarChar(1000), element.name);
-				requestDetail.input(`giaban`, sql.Float, element.price);
-				requestDetail.input(`gianhap`, sql.Float, element.importPrice);
-				requestDetail.input(`slkho`, sql.Int, element.stock);
-				requestDetail.input(`hinhanh`, sql.VarChar(5000), downloadURL);
-				// query to the database and get the records
-				requestDetail.execute("dbo.SP_ADD_DETAIL_PRODUCT", function (err, response) {
-					if (err) {
-						console.log(err);
-						res.json({ returnValue: 0 });
-					}
-				});
-			} catch (error) {
-				console.log(error);
-			}
-		}
 		sql.connect(config, function (err) {
 			if (err) console.log(err);
 
 			let id = "";
-			const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 			const charactersLength = characters.length;
 			let counter = 0;
 			while (counter < 10) {
@@ -87,173 +53,40 @@ class ManageVouchersController {
 				counter += 1;
 			}
 			// create Request object
+			console.log(req.body.data)
 			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), id);
-			request.input("tensp", sql.NVarChar(100), body.product.name);
-			request.input("mota", sql.NVarChar(1000), body.product.description);
-			request.input("brand", sql.NVarChar(30), body.product.brand);
-			request.input("tenloaisp", sql.NVarChar(1000), body.product.category);
+			request.input("voucher_id", sql.VarChar(10), id);
+			request.input("name_voucher", sql.NVarChar(10000), req.body.data.name_voucher);
+			request.input("discount", sql.Int, req.body.data.discount);
+			request.input("min_price", sql.Int, req.body.data.min_price);
+			request.input("max_price", sql.Int, req.body.data.max_price);
+			request.input("start_date", sql.DateTime, req.body.data.start_date);
+			request.input("end_date", sql.DateTime, req.body.data.end_date);
 			// query to the database and get the records
-			request.execute("dbo.SP_ADD_PRODUCT", function (err, response) {
-				if (err) console.log(err);
-				else {
-					for (let index = 0; index < body.detail.length; index++) {
-						const element = body.detail[index];
-						AddDetail(element, index, id);
-					}
-					res.json({ returnValue: 1 });
-				}
+			request.execute("dbo.SP_ADD_VOUCHER", function (err, response) {
+				if (err) res.json(err);
+				else res.json(response);
 			});
 		});
 	}
 	// [PUT] /edit/ten-san-pham
-	EditVoucher_TEN_SP(req, res) {
+	EditVoucher(req, res) {
 		sql.connect(config, function (err) {
 			if (err) console.log(err);
 
 			// create Request object
 			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("tensp", sql.NVarChar(100), req.body.data.tensp);
+			request.input("voucher_id", sql.VarChar(10), req.body.data.voucher_id);
+			request.input("name_voucher", sql.NVarChar(10000), req.body.data.name_voucher);
+			request.input("discount", sql.Int, req.body.data.discount);
+			request.input("min_price", sql.Int, req.body.data.min_price);
+			request.input("max_price", sql.Int, req.body.data.max_price);
+			request.input("start_date", sql.DateTime, req.body.data.start_date);
+			request.input("end_date", sql.DateTime, req.body.data.end_date);
 			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_PRODUCT_TEN_SP", function (err, response) {
-				if (err) console.log(err);
-				res?.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/ten-loai-san-pham
-	EditVoucher_TEN_LOAI_SP(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			console.log(req.body.data);
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("tenloaisp", sql.NVarChar(1000), req.body.data.tenloaisp);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_PRODUCT_TEN_LOAI_SP", function (err, response) {
-				if (err) console.log(err);
-				console.log(response.returnValue);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/mo-ta
-	EditVoucher_MO_TA(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("mota", sql.NVarChar(1000), req.body.data.mota);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_PRODUCT_MO_TA", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/brand
-	EditVoucher_BRAND(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("brand", sql.VarChar(30), req.body.data.brand);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_PRODUCT_BRAND", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/ten-chi-tiet-san-pham
-	EditDetailVoucher_TEN_CTSP(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("stt", sql.Int, req.body.data.stt);
-			request.input("tenctsp", sql.NVarChar(1000), req.body.data.tenctsp);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_DETAIL_PRODUCT_TEN_CTSP", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/gia-ban
-	EditDetailVoucher_GIA_BAN(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("stt", sql.Int, req.body.data.stt);
-			request.input("giaban", sql.Float, req.body.data.giaban);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_DETAIL_PRODUCT_GIA_BAN", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/gia-nhap
-	EditDetailVoucher_GIA_NHAP(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("stt", sql.Int, req.body.data.stt);
-			request.input("gianhap", sql.Float, req.body.data.gianhap);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_DETAIL_PRODUCT_GIA_NHAP", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/so-luong-kho
-	EditDetailVoucher_SL_KHO(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("stt", sql.Int, req.body.data.stt);
-			request.input("slkho", sql.Int, req.body.data.slkho);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_DETAIL_PRODUCT_SL_KHO", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
-			});
-		});
-	}
-	// [PUT] /edit/hinh-anh
-	EditDetailVoucher_HINHANHSP(req, res) {
-		sql.connect(config, function (err) {
-			if (err) console.log(err);
-
-			// create Request object
-			var request = new sql.Request();
-			request.input("masp", sql.VarChar(10), req.body.data.masp);
-			request.input("stt", sql.Int, req.body.data.stt);
-			request.input("hinhanh", sql.VarChar(5000), req.body.data.hinhanh);
-			// query to the database and get the records
-			request.execute("dbo.SP_EDIT_DETAIL_PRODUCT_HINHANHSP", function (err, response) {
-				if (err) console.log(err);
-				res.json(response);
+			request.execute("dbo.SP_EDIT_VOUCHER", function (err, response) {
+				if (err) res?.json(err);
+				else res?.json(response);
 			});
 		});
 	}
@@ -265,8 +98,8 @@ class ManageVouchersController {
 				await sql.connect(config).then((conn) =>
 					conn
 						.request()
-						.input("masp", sql.VarChar(10), req.body.masp)
-						.execute("dbo.SP_DELETE_PRODUCT")
+						.input("voucher_id", sql.VarChar(10), req.body.voucher_id)
+						.execute("dbo.SP_DELETE_VOUCHER")
 						.then((v) => {
 							result = v;
 						})
--- src/routes/ManageVouchers.js
+++ src/routes/ManageVouchers.js
@@ -7,16 +7,8 @@ const upload = multer({ storage: multer.memoryStorage() });
 const ManageVouchersController = require("../controllers/ManageVouchersController");
 
 router.get("/", ManageVouchersController.GetVouchers);
-router.post("/", upload.fields([{ name: "imageUpload", maxCount: 1 }]), ManageVouchersController.AddVoucher);
-router.put("/edit/ten-san-pham", ManageVouchersController.EditVoucher_TEN_SP);
-router.put("/edit/ten-loai-san-pham", ManageVouchersController.EditVoucher_TEN_LOAI_SP);
-router.put("/edit/mo-ta", ManageVouchersController.EditVoucher_MO_TA);
-router.put("/edit/brand", ManageVouchersController.EditVoucher_BRAND);
-router.put("/edit/ten-chi-tiet-san-pham", ManageVouchersController.EditDetailVoucher_TEN_CTSP);
-router.put("/edit/gia-ban", ManageVouchersController.EditDetailVoucher_GIA_BAN);
-router.put("/edit/gia-nhap", ManageVouchersController.EditDetailVoucher_GIA_NHAP);
-router.put("/edit/so-luong-kho", ManageVouchersController.EditDetailVoucher_SL_KHO);
-router.put("/edit/hinh-anh", ManageVouchersController.EditDetailVoucher_HINHANHSP);
+router.post("/", ManageVouchersController.AddVoucher);
+router.put("/", ManageVouchersController.EditVoucher);
 router.delete("/", ManageVouchersController.DeleteVoucher);
 
 module.exports = router;
