cf279e6394bdf669ed7f7c081c79d4f681320cf9
dat123 <dat030602@gmail.com>
2023-08-17 16:40:49 +0700

    Update add product

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const HomeController = require('../controllers/HomeController');
 
-router.get('/getBranch1', HomeController.getBranch1);
 router.get('/', HomeController.index)
 
 module.exports = router;
