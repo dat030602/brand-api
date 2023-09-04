d19ad519de464751ccd9aed54c653d7c80a6bfd3
ngoctranaa <trananhngoc2110@gmail.com>
2023-09-04 23:29:42 +0700

    Update Search route

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,13 +2,21 @@ const config = require("../DbConfig");
 const sql = require("mssql");
 
 class SearchController {
-    async index(req, res) {}
-    	// [GET]
+	async index(req, res) { }
+	// [GET]
 	GetAll(req, res) {
 		const func = async () => {
 			try {
 				let pool = await sql.connect(config);
-				let result = pool.request().query(`SELECT * FROM dbo.SANPHAM DT`);
+				let result;
+				await pool
+					.request()
+					.query(`SELECT * FROM dbo.SANPHAM DT join dbo.HINHANHSP H on DT.MA_SP = H.MA_SP join dbo.CT_SANPHAM CT on DT.MA_SP = CT.MA_SP join dbo.LOAISANPHAM L on DT.MA_LOAI_SP = L.MA_LOAI_SP
+					where H.STT = 1 and CT.STT = 1`)
+					.then((v) => {
+						result = v;
+					})
+					.then(() => pool.close())
 				return result;
 			} catch (error) {
 				console.log(`Error: ${error}`);
@@ -25,7 +33,7 @@ class SearchController {
 				let pool = await sql.connect(config);
 				let result = pool
 					.request()
-					.query(`SELECT * FROM dbo.SANPHAM DT where DT.TEN_SP like '%${req.params.slug}%'`);
+					.query(`SELECT * FROM dbo.SANPHAM DT where DT.TEN_SP like '%${req.query.key}%'`);
 				return result;
 			} catch (error) {
 				console.log(`Error: ${error}`);
@@ -40,10 +48,36 @@ class SearchController {
 		const func = async () => {
 			try {
 				let pool = await sql.connect(config);
+				let result
+				await pool
+					.request()
+					.query(
+						`SELECT * FROM dbo.SANPHAM DT join dbo.HINHANHSP H on DT.MA_SP = H.MA_SP join dbo.CT_SANPHAM CT on DT.MA_SP = CT.MA_SP join dbo.LOAISANPHAM L on DT.MA_LOAI_SP = L.MA_LOAI_SP
+						where H.STT = 1 and CT.STT = 1 and L.TEN_LOAI_SP like '${req.query.category}'`
+					)
+					.then((v) => {
+						result = v;
+					})
+					.then(() => pool.close())
+
+				return result;
+			} catch (error) {
+				console.log(`Error: ${error}`);
+			}
+		};
+		func().then((resReturn) => {
+			res.json(resReturn?.recordset);
+		});
+	}
+	// [GET]
+	GetListByCategory(req, res) {
+		const func = async () => {
+			try {
+				let pool = await sql.connect(config);
 				let result = pool
 					.request()
 					.query(
-						`SELECT * FROM dbo.SANPHAM DT join dbo.LOAISANPHAM L on DT.MA_LOAI_SP = L.MA_LOAI_SP where L.MA_LOAI_SP = '${req.params.slug}'`
+						`SELECT * FROM dbo.SANPHAM DT where DT.MA_LOAI_SP = '${req.query.type}' and DT.TEN_SP like '%${req.query.key}%'`
 					);
 				return result;
 			} catch (error) {
@@ -55,15 +89,67 @@ class SearchController {
 		});
 	}
 	// [GET]
-	GetListByCategory(req, res) {
+	GetAllBrand(req, res) {
 		const func = async () => {
 			try {
 				let pool = await sql.connect(config);
-				let result = pool
+				let result;
+				await pool
 					.request()
-					.query(
-						`SELECT * FROM dbo.SANPHAM DT join dbo.LOAISANPHAM L on DT.MA_LOAI_SP = L.MA_LOAI_SP where L.MA_LOAI_SP = '${req.params.slug}' and DT.TEN_SP like '%${req.params.slug1}%'`
-					);
+					.query(`SELECT distinct BRAND FROM dbo.SANPHAM`)
+					.then((v) => {
+						result = v;
+					})
+					.then(() => pool.close())
+				return result;
+			} catch (error) {
+				console.log(`Error: ${error}`);
+			}
+		};
+		func().then((resReturn) => {
+			res.json(resReturn.recordset);
+		});
+	}
+		// [GET]
+	GetAll(req, res) {
+		const func = async () => {
+			try {
+				let pool = await sql.connect(config);
+				let result;
+				await pool
+					.request()
+					.query(`SELECT * FROM dbo.SANPHAM DT join dbo.HINHANHSP H on DT.MA_SP = H.MA_SP join dbo.CT_SANPHAM CT on DT.MA_SP = CT.MA_SP join dbo.LOAISANPHAM L on DT.MA_LOAI_SP = L.MA_LOAI_SP
+					where H.STT = 1 and CT.STT = 1`)
+					.then((v) => {
+						result = v;
+					})
+					.then(() => pool.close())
+				return result;
+			} catch (error) {
+				console.log(`Error: ${error}`);
+			}
+		};
+		func().then((resReturn) => {
+			res.json(resReturn.recordset);
+		});
+	}
+	// [GET]
+	GetAllByFilter(req, res) {
+		const func = async () => {
+			try {
+				let pool = await sql.connect(config);
+				let result;
+				await pool
+					.request()
+					.input("MA_LOAI_SP", sql.VarChar(10), req.query.type)
+					.input("BRAND", sql.VarChar(30), req.query.brand)
+					.input("PRICE", sql.VarChar(10), req.query.price)
+					.input("RATING", sql.VarChar(10), req.query.rating)
+					.execute("dbo.SP_FILTER_PRODUCTS")
+					.then((v) => {
+						result = v;
+					})
+					.then(() => pool.close())
 				return result;
 			} catch (error) {
 				console.log(`Error: ${error}`);
