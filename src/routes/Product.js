85bd3f3b07322814137754b752bcd9f7f7990a59
dat123 <dat030602@gmail.com>
2023-09-07 21:53:38 +0700

    Add To Cart, Favorite

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const ProductController = require("../controllers/ProductController");
 
 router.get("/", ProductController.GetProduct);
+router.post("/add-to-cart", ProductController.AddToCart);
+router.post("/favorite", ProductController.Favorite);
 
 module.exports = router;
